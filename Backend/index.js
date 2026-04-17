2c400de7cc361089e78a416bd41be8d9fd89fbde
SajadHaider123 <sajjadhaider0094@gmail.com>
2026-04-18 04:43:36 +0500

    Initial commit: Adding Full Stack Project files

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express());
+
+app.get('/api/raw_call_logs', (req, res) => {
+    res.json([
+        {id: 1, phone: "03482311676"},
+        {id: 2, phone: "03234567676"},
+
+    ])
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
